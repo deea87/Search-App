b116da70f857320e24ab6155d08a1f23bd17609a
Andreea <113915219+deea87@users.noreply.github.com>
2023-05-12 15:46:56 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,62 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(
+          `https://api.api-ninjas.com/v1/celebrity?name=${searchTerm}`,
+          {
+            headers: {
+              "X-Api-Key": "Write your Api KEY here",
+            },
+          }
+        );
+
+        const data = await res.json();
+        setSearchResults(data);
+      } catch (error) {
+        setError(error);
+      }
+      setIsLoading(false);
+    };
+    if (searchTerm.length > 0) {
+      fetchData();
+    }
+  }, [searchTerm]);
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="search">Search for a Nepo Baby:</label>
+        <input
+          type="text"
+          id="search"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+      </form>
+      {error && <div>{error.message}</div>}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        searchResults.map((result) => (
+          <div key={result.id}>
+            <h2>{result.name}</h2>
+            <p>{result.max_height}</p>
+            <p>Nepo Baby Net worth from 2023: {result.net_worth}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
+}
+
+export default App;
